9f887ac59ee1397e80bd5afbab3e3ebe9a52a29a
Shreyas-6403 <kulkarni6403@gmail.com>
2021-08-13 13:05:35 +0530

    13AugCommit

---
+++ 13AugPr.py
@@ -0,0 +1,36 @@
+# Identify the Valid and invalid Operators?
+
+      # < > = Invalid
+      # >=  = valid
+      #//=  = valid
+      # &   = valid
+
+#If i = 3, j = 2 what is the result of following expressions?
+
+i=3
+j=2
+print ("\n i + 5 >= j – 6",i + 5 >= j - 6) 
+print ("\n j * 10 < i ** 2",j * 10 < i ** 2)
+print ("\n i < j + 5 > j ** 4",i < j + 5 > j ** 4)
+print("\n")
+
+# Which is the correct operator for power(xy)? =  X**y
+# Which one of these is floor division? = //
+# What is the order of precedence in python? = i) Parentheses ,ii) Exponential iii) Multiplication iv) Division v) Addition vi) Subtraction
+#What is the answer to this expression, 22 % 3 is? = 1
+
+#What is meant by an Operator in Python?
+
+#   Ans. Operators are used to perform operations on variables and values.
+#   For example, we use the + operator to add together two values.
+
+
+# How many types of operators in Python?
+
+# Arithmetic operators 
+# Assignment operators
+# Comparison operators
+# Logical operators
+# Identity operators
+# Membership operators
+# Bitwise operators
